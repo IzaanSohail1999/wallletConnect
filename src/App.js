d1b6f085dd8686ec6d1a8def4054ba41eb03e55c
Izaan Sohail <izaansohail1999@gmail.com>
2022-08-15 18:39:50 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core'
+const { ethereum } = window;
+
+
+const CoinbaseWallet = new WalletLinkConnector({
+  url: `https://rinkeby.infura.io/v3/f9393070d836406d882c48e92469b634`,
+  appName: "Web3-react Demo",
+  supportedChainIds: [1, 3, 4, 5, 42],
+ });
+ 
+ 
+//  const Injected = new InjectedConnector({
+//   supportedChainIds: [1, 3, 4, 5, 42]
+//  });
 
 function App() {
+
+  async function metamask(){
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    // setCurrentAccount(accounts[0]);
+    // activate(Injected)
+    console.log(accounts[0]);
+  }
+
+  const { activate, active, chainId, account, deactivate } = useWeb3React();
+  // console.log(active)
+  // console.log(chainId)
+  // console.log(account)
+  // console.log(Injected)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
+      {/* <button onClick={() => { activate(WalletConnect) }}>Wallet Connect</button> */}
+      <button onClick={() => {metamask()}}>Metamask</button>
+      <button onClick={deactivate}>Disconnect</button>
+      <div>Connection Status: {active}</div>
+    <div>Account: {account}</div>
+    <div>Network ID: {chainId}</div>
     </div>
   );
 }
